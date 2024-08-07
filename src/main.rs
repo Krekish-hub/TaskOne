753cd025a395a99468834d378188ba8cd94e59d2
krek <krekish@list.ru>
2024-08-07 20:10:16 +0900

    view "Folder"

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
 use std::fs;
+use std::io::{self, Read};
+use std::path::{Path, PathBuf};
 
-fn main() {
-    let paths = fs::read_dir("./Folder").unwrap();
-    for path in paths {
-        println!("Name: {}", path.unwrap().path().display())
+fn read_dir_recursive(dir: &Path) -> io::Result<()> {
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() {
+                read_dir_recursive(&path)?;
+            } else if path.extension().and_then(|s| s.to_str()) == Some("txt") {
+                println!("Name: {:?}", path);
+            }
+        }
     }
+    Ok(())
+}
+
+fn main() -> io::Result<()> {
+    read_dir_recursive(Path::new("./Folder"))?;
+    Ok(())
 }
