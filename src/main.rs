812ec44a2d0b4e791e35ce1b557098b3f2d95cf0
krek <krekish@list.ru>
2024-08-08 01:14:41 +0900

    read files and dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,38 @@
+use std::collections::HashMap;
 use std::fs;
-use std::io::{self, Read};
+use std::io::{self, Read, read_to_string};
 use std::path::{Path, PathBuf};
 
-fn read_dir_recursive(dir: &Path) -> io::Result<()> {
-    if dir.is_dir() {
+fn read_dir_recursive(
+    dir: &Path,
+    files_content: &mut HashMap<String, String>,
+    dependencies: &mut HashMap<String, Vec<String>>,
+) -> io::Result<()>
+{
+    if dir.is_dir()
+    {
         for entry in fs::read_dir(dir)? {
             let entry = entry?;
             let path = entry.path();
             if path.is_dir() {
-                read_dir_recursive(&path)?;
+                read_dir_recursive(&path, files_content, dependencies)?;
             } else if path.extension().and_then(|s| s.to_str()) == Some("txt") {
-                println!("Name: {:?}", path);
+                let mut content = String::new();
+                fs::File::open(&path)?.read_to_string(&mut content)?;
+
+                let file_path = path.strip_prefix("./").unwrap().to_str().unwrap().to_string();
+                files_content.insert(file_path.clone(), content.clone());
+
+                let mut deps = Vec::new();
+                for line in content.lines()
+                {
+                    if line.starts_with("*require '") && line.ends_with("'*")
+                    {
+                        let dep = line[10..line.len()-2].to_string();
+                        deps.push(dep);
+                    }
+                }
+                dependencies.insert(file_path, deps);
             }
         }
     }
@@ -18,6 +40,13 @@ fn read_dir_recursive(dir: &Path) -> io::Result<()> {
 }
 
 fn main() -> io::Result<()> {
-    read_dir_recursive(Path::new("./Folder"))?;
+    let mut files_content = HashMap::new();
+    let mut dependencies = HashMap::new();
+    read_dir_recursive(Path::new("./Folder"), &mut files_content, &mut dependencies)?;
+
+    for (file, deps) in &dependencies
+    {
+        println!("Name: {}, Dependencies: {:?}", file, deps);
+    }
     Ok(())
 }
